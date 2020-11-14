4b41481fbeafcbe27754df292b186ab04408ee68
alaqsaka <aqsaabidin15@gmail.com>
2020-11-14 13:11:56 +0700

    stuck_in_a_time_loop

---
+++ stuck_in_a_time_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	int N;
+	cin >> N;
+	if(N>=1 && N <= 100)
+	{
+		int i; 
+		for(i=1; i<=N; i++){
+			cout << "" << i << " Abracadbra" << endl;
+		}
+	}
+}
